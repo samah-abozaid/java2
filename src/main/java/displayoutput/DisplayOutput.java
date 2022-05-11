e0a62d7a36fee0274be836bc7e0c6e299efc4354
samah-abozaid <samah.abozaid89@gmail.com>
2022-05-11 13:06:23 +0200

    ffff

--- src/main/java/displayoutput/DisplayOutput.java
+++ src/main/java/displayoutput/DisplayOutput.java
@@ -1,6 +1,8 @@
 package displayoutput;
 
 import java.util.Scanner;
+import java.util.InputMismatchException;
+
 public class DisplayOutput {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
@@ -9,15 +11,28 @@ public class DisplayOutput {
         int num2;
         int num3;
         int num4;
+        try {
 
-        System.out.println("enter num1");
-        num1=input.nextInt();
-        System.out.println("enter num2");
-        num2= input.nextInt();
-        num3= num1 +num2;
-        System.out.println(num3);
-        num4=num1*num2;
-        System.out.println(num4);
+
+            System.out.println("enter num1");
+            num1 = input.nextInt();
+            System.out.println("enter num2");
+            num2 = input.nextInt();
+
+            num3 = num1 + num2;
+            System.out.println(num3);
+            num4 = num1 * num2;
+            System.out.println(num4);
+        }
+        catch (InputMismatchException e) {
+            System.out.println("Attention ! Tu es sencé mettre un nombre entier");
+        }
+        catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+
+
+
 
 
 
